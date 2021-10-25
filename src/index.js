56bd174d9cce2b7f1e683410da7da1b67cdb881d
Mesi <mse212002@gmail.com>
2021-10-25 23:09:31 +0300

    :construction: working on basic features

--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
-import _ from 'lodash';
 import './style.css';
+import createEle from './elements/createEle.js';
+import createTaskArea from './elements/createTaskArea.js';
 
- function component() {
-   const element = document.createElement('div');
+const todoList = [
+  {
+    description: 'task1',
+    completed: false, 
+    index: 1,
+  },
+  {
+    description: 'task2',
+    completed: false, 
+    index: 2,
+  },
+  { 
+    description: 'task3',
+    completed: false, 
+    index: 3,
+  },
+];
 
-  // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-   element.classList.add('hello');
-   return element;
- }
+const displayTasks = () => {
+  const mainArea = createTaskArea();
+  const list = createEle('ul', 'todos', 'list-todos', null);
+  const clearAllFinished = createEle('div', null, 'clear', 'Clear all completed');
+  todoList.forEach((task) => {
+    const row = createEle('li', null, `task-${task.index}`, null);
+    const label = createEle('label', null, null, task.description);
+    const input = createEle('input', null, task.index, null);
+    const dots = createEle('div', `${task.completed === true ? 'trash' : 'dot'}`, `dot-${task.index}`, null);
+    input.setAttribute('type', 'checkbox');
+    input.setAttribute('value', task.description);
+    label.htmlFor = `${task.index}`;
+    row.appendChild(input);
+    row.appendChild(label);
+    row.appendChild(dots);
+    list.appendChild(row);
+  });
+  clearAllFinished.setAttribute('id', 'clear');
+  mainArea.appendChild(list);
+  mainArea.appendChild(clearAllFinished);
+  return mainArea;
+};
 
- document.body.appendChild(component());
+displayTasks();
