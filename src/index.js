4429e843fba66ecf653958a6f4e31c99028ca5f7
Mesi <mse212002@gmail.com>
2021-10-26 16:04:58 +0300

    :rotating_light: fixed linter errors

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 import './style.css';
 import createEle from './elements/createEle.js';
 import createTaskArea from './elements/createTaskArea.js';
-import '@fortawesome/fontawesome-free/js/all.js';
 
 const todoList = [
   {
     description: 'task1',
-    completed: false, 
+    completed: false,
     index: 1,
   },
   {
     description: 'task2',
-    completed: false, 
+    completed: false,
     index: 2,
   },
   { 
     description: 'task3',
-    completed: false, 
+    completed: false,
     index: 3,
   },
 ];
@@ -29,7 +28,6 @@ const displayTasks = () => {
     const row = createEle('li', null, `task-${task.index}`, null);
     const label = createEle('label', null, null, task.description);
     const input = createEle('input', null, task.index, null);
-    console.log(input)
     const dots = createEle('div', `${task.completed === true ? 'trash' : 'dot'}`, `dot-${task.index}`, null);
     input.setAttribute('type', 'checkbox');
     input.setAttribute('value', task.description);
--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,8 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
-      },{
+      },
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
