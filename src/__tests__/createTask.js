1b522fd704deed7daa8bc3d33f783dea37651f93
karelvanoordt <63868555+karelvanoordt@users.noreply.github.com>
2021-11-04 10:20:20 -0500

    :white_check_mark: Test if li elements are created

--- src/__tests__/createTask.js
+++ src/__tests__/createTask.js
@@ -1,8 +1,45 @@
+import { jsxEmptyExpression } from '@babel/types';
 import createTask from '../functionalities/createTask.js';
-import tasks from '../__mocks__/createTask.js'
+import deleteTask from '../functionalities/deleteTask.js';
+import tasks from '../__mocks__/createTask.js';
 
-test('adds a new task ', () => {
-  const val = 'test task';
-  createTask(tasks, val);
-  expect(tasks.length).toBe(3);
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+const dom = new JSDOM(`<!DOCTYPE html><div id="main"></div>`);
+const document = dom.window.document;
+
+describe('Test add and remove methods', () => {
+
+  it('adds a new task ', () => {
+    const val = 'test task';
+    createTask(tasks, val);
+    expect(tasks.length).toBe(3);
+  });
+
+  it('adds a new task ', () => {
+    const val = 'test task 2';
+    createTask(tasks, val);
+    expect(tasks.length).toBe(4);
+  });
+
+  it('checks if value matches the description', () => {
+    const val = 'test task 3';
+    createTask(tasks, val);
+    expect(tasks[4].description).toEqual(val);
+  });
+
+  it('checks if li element is created', () => {
+    for (let j = 0; j < tasks.length; j++)    {
+      const main = document.querySelector('#main');
+      const li = document.createElement('li');
+      li.innerHTML = `<input type="checkbox" id="checkbox">${tasks[j].description}`;
+      main.appendChild(li);
+      const liList = main.getElementsByTagName('li');
+      expect((liList.length).value).toBe((tasks.length).value);
+    }
+  });
+
 });
+
+
